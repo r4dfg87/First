307dadec58ca6ab2533eff6fee53526702b9d94f
Quadir Cooke <quadirr.cooke@gmail.com>
2022-05-07 00:25:05 -0400

    Initial commit

---
+++ pythonProject/main.py
@@ -0,0 +1,36 @@
+class linkedListNode:
+    def __init__(self, value, nextNode=None):
+        self.value = value
+        self.nextNode = nextNode
+
+
+class linkedList:
+    def __init__(self, head=None):
+        self.head = head
+
+    def insert(self, value):
+        node = linkedListNode(value)
+        if self.head is None:
+            self.head = node
+            return
+
+        currentNode = self.head
+        while True:
+            if currentNode.nextNode is None:
+                currentNode.nextNode = node
+                break
+            currentNode = currentNode.nextNode
+
+    def printLinkedList(self):
+        currentNode = self.head
+        while currentNode is not None:
+            print (currentNode.value, "->", end="  ")
+            currentNode = currentNode.nextNode
+        print ("None")
+
+
+
+ll = linkedList()
+ll.insert("3")
+ll.insert("9")
+ll.printLinkedList()
